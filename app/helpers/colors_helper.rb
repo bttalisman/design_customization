7b491a9617477895efd394915d98955f572029d0
Benjamin Talisman <bttalisman@gmail.com>
2016-04-16 14:56:54 -0700

    extracted color swatch

--- app/helpers/colors_helper.rb
+++ app/helpers/colors_helper.rb
@@ -1,3 +1,17 @@
 # Colors Helper
 module ColorsHelper
+
+  def swatch( c, settings )
+
+    logger.info 'ColorsHelper - swatch() - settings: ' + settings.to_s
+
+    display = settings[:display]
+    id = settings[:id]
+
+    s = '<div class="color-item" id="' + id.to_s + '" title="'\
+      + c.hex_code.to_s + ' - ' + c.description.to_s\
+      + '" style="display:' + display.to_s + '; background:' + c.hex_code.to_s\
+      + '"></div>'
+    s
+  end
 end
