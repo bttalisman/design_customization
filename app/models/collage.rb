bf39455a3e83070e7e153e2d7eafd12eca9e19bc
Benjamin Talisman <bttalisman@gmail.com>
2016-04-20 12:40:41 -0700

    illustrator collage components

--- app/models/collage.rb
+++ app/models/collage.rb
@@ -11,7 +11,8 @@ class Collage < ActiveRecord::Base
   # contacts Instagram and downloads images based on the query into that
   # folder.
   def build_collage_folder
-    path = @@path_to_collage_root + '/collage_' + self[:id].to_s
+#    path = @@path_to_collage_root + '/collage_' + self[:id].to_s
+    path = @@path_to_collage_root + '/test_collage'
     logger.info 'COLLAGE - build_collage_folder() - path: ' + path.to_s
 
     self[:path] = path
