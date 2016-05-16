0bafbb5169ebbaf6c8b2d416b4196c3629845bae
Benjamin Talisman <bttalisman@gmail.com>
2016-05-16 12:52:24 -0700

    added json2.jsx

--- test/controllers/colors_controller_test.rb
+++ test/controllers/colors_controller_test.rb
@@ -1,7 +1,30 @@
-require 'test_helper'
+require_relative '../helpers/test_helper'
 
+# Colors Controlle Test
 class ColorsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include TestHelper
+
+  test 'update action' do
+    c = Color.new
+    c.save
+    id = c.id
+
+    get( :update, { 'id' => id,
+                    'color' => { 'hex_code' => '#aaaaaa',
+                                 'description' => 'test desc' } } )
+
+    path = color_path( c.id )
+    Rails.logger.info 'ColorsControllerTest - update_action() - path: '\
+      + path.to_s
+    assert_redirected_to path
+  end
+
+  test 'delete all' do
+    10.times do
+      c = Color.new
+      c.save
+    end
+    get( :delete_all, {} )
+    assert_response :success
+  end
 end
--- test/models/replacement_image_test.rb
+++ test/models/replacement_image_test.rb
@@ -1,7 +1,16 @@
-require 'test_helper'
+require_relative '../helpers/test_helper'
 
+# Test ReplacementImage model
 class ReplacementImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include TestHelper
+  test 'filename' do
+    file = sample_file( 'one_tag_one_image.ai' )
+    ri = ReplacementImage.new
+    ri.uploaded_file = file
+    ri.save
+    fn = ri.file_name
+
+    assert_equal( 'one_tag_one_image.ai', fn,\
+                  'ReplacementImage.filename function isnt working!' )
+  end
 end
