3b3be076ed0792f943f6b8b8e25b0f2c00998bae
Benjamin Talisman <bttalisman@gmail.com>
2016-05-17 10:44:44 -0700

    Added color and palette tests

--- test/controllers/colors_controller_test.rb
+++ test/controllers/colors_controller_test.rb
@@ -20,11 +20,35 @@ class ColorsControllerTest < ActionController::TestCase
   end
 
   test 'delete all' do
+    get( :delete_all, {} )
+    assert_response :success
+
     10.times do
       c = Color.new
       c.save
     end
+
+    count = Color.all.count
+    assert_equal( 10, count, 'Unexpected color count.' )
+
     get( :delete_all, {} )
     assert_response :success
+
+    count = Color.all.count
+    assert_equal( 0, count, 'Unexpected color count.' )
   end
+
+  test 'create behavior' do
+    original_count = Color.all.count
+    color_params = { color: { description: 'test color',
+                              hex_code: '#000000' } }
+
+    get( :create, color_params )
+    assert_redirected_to( @color )
+
+    new_count = Color.all.count
+
+    assert_equal( original_count + 1, new_count, 'Unexpected color count.' )
+  end
+
 end
--- test/controllers/palettes_controller_test.rb
+++ test/controllers/palettes_controller_test.rb
@@ -1,7 +1,83 @@
-require 'test_helper'
+require_relative '../helpers/test_helper'
 
+# Palettes Controller Test
 class PalettesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include TestHelper
+
+  test 'update action' do
+    p = Palette.new
+    p.save
+    id = p.id
+
+    get( :update, id: id,
+                  palette: { description: 'test palette description',
+                             name: 'test name' } )
+
+    path = palette_path( p.id )
+    assert_redirected_to path
+  end
+
+  test 'delete all' do
+    get( :delete_all, {} )
+    assert_response :success
+
+    10.times do
+      p = Palette.new
+      p.save
+    end
+
+    count = Palette.all.count
+    assert_equal( 10, count, 'Unexpected palette count.' )
+
+    get( :delete_all, {} )
+    assert_response :success
+
+    count = Palette.all.count
+    assert_equal( 0, count, 'Unexpected palette count.' )
+  end
+
+  test 'create behavior' do
+    original_count = Palette.all.count
+    palette_params = { palette: { name: 'test name',
+                                  description: 'test palette' } }
+
+    get( :create, palette_params )
+    assert_redirected_to( @palette )
+
+    new_count = Palette.all.count
+
+    assert_equal( original_count + 1, new_count, 'Unexpected palette count.' )
+  end
+
+  test 'add and remove behavior' do
+    p = Palette.new
+    p.save
+    p_id = p.id
+
+    # Add 10 colors
+    10.times do
+      c = Color.new
+      c.save
+
+      get( :add, id: p_id,
+                 color_id: c.id )
+      assert_response :success
+    end
+    assert_equal( 10, p.colors.count, 'Unexpected color count.' )
+
+    # Remove the first one
+    id_to_remove = p.colors.first.id
+    get( :remove, id: p_id,
+                  color_id: p.colors.first.id )
+    assert_response :success
+    assert_equal( 9, p.colors.count, 'Unexpected color count.' )
+
+    # Remove all colors
+    get( :remove_all, id: p_id )
+    assert_response :success
+    assert_equal( 0, p.colors.count, 'Unexpected color count.' )
+
+    assert( p.save, 'Save failed.' )
+    
+  end
 end
