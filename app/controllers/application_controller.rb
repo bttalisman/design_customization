f20dca51fb97d94efd9789452e54cb6df8ed1e03
Benjamin Talisman <bttalisman@gmail.com>
2016-06-20 10:17:06 -0700

    added fetch and merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # This action is invoked to remotely cause the rails server to get the latest
+  # code from the git repo.
   def git_fetch
-    Rails.logger.info( 'ApplicationController - git_fetch()' )
     sys_com = 'git fetch'
     success = system( sys_com )
+    Rails.logger.info( 'ApplicationController - git_fetch() sys_com: '\
+      + sys_com.to_s )
     Rails.logger.info( 'ApplicationController - git_fetch() success: '\
       + success.to_s )
+
+    sys_com = 'git merge -m "Remotely requested merge."'
+    success = system( sys_com )
+    Rails.logger.info( 'ApplicationController - git_fetch() sys_com: '\
+      + sys_com.to_s )
+    Rails.logger.info( 'ApplicationController - git_fetch() success: '\
+      + success.to_s )
+
     render nothing: true
   end
 end
