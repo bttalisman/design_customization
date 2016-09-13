c58d45cab277cc854e6a14fe97204af9adfe81e8
Benjamin Talisman <bttalisman@gmail.com>
2016-09-13 11:52:37 -0700

    fixed date sorting bug.  and fit checkbox bug

--- app/assets/javascripts/util.js
+++ app/assets/javascripts/util.js
@@ -3,6 +3,7 @@
   function my_sort( obj1, obj2, key, flip )
   {
     var o1, o2;
+    var d1, d2;
 
     if( flip )
     {
@@ -17,11 +18,21 @@
 
     var s1 = '';
     if( o1[key] !== null ) {
-      s1 = o1[key].toLowerCase(); }
+      s1 = o1[key].toLowerCase();
+      d1 = Date.parse( s1 );
+    }
 
     var s2 = '';
     if( o2[key] !== null ) {
-      s2 = o2[key].toLowerCase(); }
+      s2 = o2[key].toLowerCase();
+      d2 = Date.parse( s2 );
+    }
+
+
+    if( d1 && d2 ) {
+      s1 = d1;
+      s2 = d2;
+    }
 
     var comp;
     if( s1 < s2 )
